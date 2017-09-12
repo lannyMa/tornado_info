559c26ebda069189fd9ea0e91558ef8644f118dc
maxiaolang <iher@foxmail.com>
2017-09-12 08:05:46 +0800

    json

---
+++ 09ajax&jsonp/07ajax跨域请求-域1-通过jsonp-江西卫视/start.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+
+import tornado.web
+import tornado.ioloop
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        # self.write("Hello, world")
+        self.render("index.html")
+
+    def post(self, *args, **kwargs):
+        self.write("domain1 post")
+
+
+settings = {
+    'template_path': 'templates',
+    'static_path': 'statics',
+    'static_url_prefix': '/sss/',
+    'cookie_secret': "asfsdfasdf",
+    # 'xsrf_cookies': True
+}
+
+application = tornado.web.Application([
+    (r"/index", MainHandler),
+], **settings)
+
+if __name__ == "__main__":
+    print("http://www.d1.com:8001/index")
+    application.listen(8001)
+    tornado.ioloop.IOLoop.instance().start()
